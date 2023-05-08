477760940220a76ce6b88042e492e271e5f2fce6
saxon <s3724120@student.rmit.edu.au>
2023-05-08 16:07:28 +1000

    implemented add exercise button to add new row to recylerview tables, partly implemented save button functionality

--- app/src/main/java/com/example/workouttracker/WeightsInput.java
+++ app/src/main/java/com/example/workouttracker/WeightsInput.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,12 +24,18 @@ public class WeightsInput extends AppCompatActivity {
     SimpleDateFormat dateFormat;
     String date;
     ArrayList<WeightsRecord> weightsWorkout;
+
+    WeightsAdapter wAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weights_input);
+
         // recieve intent from previous activity
         Intent receiving = getIntent();
+
+        // will implement here an if statemnet checking the intent, weather it comes from
+        // new workout button or press on home screen list to see previous workout
 
         // https://medium.com/@shayma_92409/display-the-current-date-android-studio-f582bf14f908
         // to display the date at the top of the screen
@@ -49,7 +56,7 @@ public class WeightsInput extends AppCompatActivity {
         // Initialize weights records - this will be changed for the database
         weightsWorkout = WeightsRecord.createWeightsWorkout(10);
         // Create adapter and pass workout records
-        WeightsAdapter wAdapter = new WeightsAdapter(weightsWorkout);
+        wAdapter = new WeightsAdapter(weightsWorkout);
         // Attach adapter to the recyclerview to populate
         rvWeights.setAdapter(wAdapter);
         // layout manager position the records
@@ -69,5 +76,22 @@ public class WeightsInput extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void addExercise(View view){
+        // add new empty record
+        weightsWorkout.add(new WeightsRecord());
+        // notify the adapter the adapter
+        wAdapter.notifyDataSetChanged();
+
+    }
+
+    public void saveWeightsWorkout(View view){
+        for(int i = 0; i < weightsWorkout.size(); i++){
+            weightsWorkout.get(i);
+            // add each record to respective tables
+        }
+
     }
 }
--- app/src/main/java/com/example/workouttracker/WeightsWorkout.java
+++ app/src/main/java/com/example/workouttracker/WeightsWorkout.java
@@ -22,4 +22,6 @@ public class WeightsWorkout {
 
         return mWeightsRecord;
     }
+
+
 }
