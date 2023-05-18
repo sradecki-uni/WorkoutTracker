9f7748419b880a8970aea9c4608d54e86efb2140
aisho26 <s3781589@student.rmit.edu.au>
2023-05-18 20:58:16 +1000

    Update DBHandler.java

--- app/src/main/java/com/example/workouttracker/DBHandler.java
+++ app/src/main/java/com/example/workouttracker/DBHandler.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
+import java.util.Date;
 
 import androidx.annotation.Nullable;
 // import
