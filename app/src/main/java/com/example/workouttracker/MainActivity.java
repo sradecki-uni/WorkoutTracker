78fddb63d33019dbc6d461cffabe4ea1bcbdb65a
saxon <s3724120@student.rmit.edu.au>
2023-05-20 16:30:22 +1000

    fixed issue with merge

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final String WORKOUT_ID = "com.example.workouttracker.workoutid";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void newWeightsWorkout(View view){
         Intent myIntent = new Intent(this, WeightsInput.class);
+        // testing passing workout ID with intent
+        // if your database doesnt have atleast 24 wokouts and the newest one isnt a weights workout
+        // the following line wont work
 
+        // myIntent.putExtra(WORKOUT_ID, "24")
+
+        // pass 0 if new workout
+        myIntent.putExtra(WORKOUT_ID, "0");
         startActivity(myIntent);
     }
 
